6fb981698f375bdeca86aa2687d19d724220b237
Franco Renzulli <85003859+franrenzulli@users.noreply.github.com>
2024-02-19 23:01:05 -0300

    Add files via upload

--- src/Leaderboard.cpp
+++ src/Leaderboard.cpp
@@ -14,6 +14,23 @@ Leaderboard::Leaderboard() {
 	m_t1.setCharacterSize(40);
 	m_t1.setPosition(600, 100);
 	
+	m_t2.setFont(m_f1);
+	m_t2.setFillColor(Color(255,255,255));
+	m_t2.setCharacterSize(40);
+	m_t2.setPosition(50, 350);
+	
+	m_t3.setFont(m_f1);
+	m_t3.setFillColor(Color(255,255,255));
+	m_t3.setString("Escriba el nombre del ganador");
+	m_t3.setCharacterSize(15);
+	m_t3.setPosition(50, 300);
+	
+	m_t4.setFont(m_f1);
+	m_t4.setFillColor(Color(255,255,255,0));
+	m_t4.setString("Presiona ENTER para guardar");
+	m_t4.setCharacterSize(15);
+	m_t4.setPosition(50, 430);
+	
 	m_tex_background.loadFromFile("../assets/images/portada3.png");
 	m_spr_background.setTexture(m_tex_background);
 	m_spr_background.setScale(1.1, 1.03);
@@ -25,6 +42,23 @@ Leaderboard::~Leaderboard() {}
 void Leaderboard::ProcessEvents(Game &game, Event &event) { // Volver al menu
 	if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {		
 		game.SetScene(new Menu());
+	}else if (event.type == sf::Event::TextEntered) { // Escribir tu nombre en la pantalla
+		if (event.text.unicode < 128) {
+			if (event.text.unicode == 8 && !currentInput.empty()) {
+				// Retroceso: eliminar el último carácter
+				currentInput.pop_back();
+			} else if (event.text.unicode != 8 && currentInput.length() < 8) {
+				// Agregar el carácter ingresado
+				currentInput += static_cast<char>(event.text.unicode);
+			}
+			m_t2.setString(currentInput);
+		}
+		// Hacer aparecer o desaparecer el texto de guardar
+		if(currentInput.length() > 0){
+			m_t4.setFillColor(Color(255,255,255,100));
+		}else{
+			m_t4.setFillColor(Color(255,255,255,0));
+		}
 	}
 }
 
@@ -34,6 +68,9 @@ void Leaderboard::Draw(RenderWindow &window) { // Mostramos los fondos y textos
 	window.clear({0,0,0});
 	window.draw(m_spr_background);
 	window.draw(m_t1);
+	window.draw(m_t2);
+	window.draw(m_t3);
+	window.draw(m_t4);
 	window.display();
 }
 
--- src/Leaderboard.h
+++ src/Leaderboard.h
@@ -1,8 +1,9 @@
 #pragma once
-
 #include <SFML/Graphics.hpp>
 #include "Game.h"
 #include "Scene.h"
+
+using namespace std;
 
 class Leaderboard : public Scene { // Creamos la escena Leaderboard
 public:
@@ -15,6 +16,10 @@ public:
 private:
 	Font m_f1;
 	Text m_t1;
+	Text m_t2; // Texto donde se escribira el nombre 
+	Text m_t3;
+	Text m_t4;
+	string currentInput;
 	Texture m_tex_background;
 	Sprite m_spr_background;
 };
